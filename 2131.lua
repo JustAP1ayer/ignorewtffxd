00ba064670266e7987864d3c61edd71a00ecb99c
JustAP1ayer <70037178+JustAP1ayer@users.noreply.github.com>
2023-05-17 20:29:19 -0400

    Create 2131.lua

---
+++ 2131.lua
@@ -0,0 +1,35 @@
+
+                 local gold = workspace:WaitForChild("ClaimRiverResultsGold")
+ repeat wait() until game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+  _G.AdvGoldTpFarm2 = true
+    local function advcgoldtp()
+        for i = 1, 10 do
+            game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = workspace:WaitForChild("BoatStages"):WaitForChild("NormalStages")["CaveStage"..i].DarknessPart.CFrame 
+             wait(2)
+             end
+            if i == 5 then
+                if game:GetService("Players").LocalPlayer.Character  and game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart") and game:GetService("Players").LocalPlayer.Character.Humanoid.Health > 0 then
+                    firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, 0)
+                    firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, 0)
+                    firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, 0)
+                    firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, 1)
+                    firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, 0)
+                    firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, 0)
+                end
+            end
+            if i == 8 then
+                if game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
+                    firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, workspace:WaitForChild("BoatStages"):WaitForChild("NormalStages").TheEnd.GoldenChest.Trigger, 1)
+                firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, workspace:WaitForChild("BoatStages"):WaitForChild("NormalStages").TheEnd.GoldenChest.Trigger, 0)
+                firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, workspace:WaitForChild("BoatStages"):WaitForChild("NormalStages").TheEnd.GoldenChest.Trigger, 1)
+            end
+            end
+    end
+    
+        game:GetService("RunService").Stepped:connect(
+            function()
+                if _G.AdvGoldTpFarm2 then
+                    game.Players.LocalPlayer.Character:WaitForChild("Humanoid"):ChangeState(11)
+                end
+            end)
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/JustAP1ayer/ignorewtffxd/main/serverhopxd.lua", true))()
