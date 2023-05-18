57aa92a7fc3679ad23ed41e4b6f8cba23e2d133b
JustAP1ayer <70037178+JustAP1ayer@users.noreply.github.com>
2023-05-17 20:52:58 -0400

    Update 2131.lua
    i hate my life why does it crash

--- 2131.lua
+++ 2131.lua
@@ -1,37 +1,37 @@
- repeat wait(0.1) until game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
-                 local gold = workspace:WaitForChild("ClaimRiverResultsGold")
-        game:GetService("RunService").Stepped:connect(
-            function()
-                    game.Players.LocalPlayer.Character:WaitForChild("Humanoid"):ChangeState(11)
-            end)
-
-                  
-   gold:FireServer()
-            local bodyVelocity = Instance.new("BodyVelocity")
-            bodyVelocity.Velocity = Vector3.new(0, 0, 0)
-            bodyVelocity.Parent = game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart")
-             for i = 1, 10 do
-            game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = workspace:WaitForChild("BoatStages"):WaitForChild("NormalStages")["CaveStage"..i].DarknessPart.CFrame 
-             wait(2)
-             end
-            if i == 5 then
-                if game:GetService("Players").LocalPlayer.Character  and game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart") and game:GetService("Players").LocalPlayer.Character.Humanoid.Health > 0 then
-                    firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, 0) -- it works doe!!!
-                    firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, 0)
-                    firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, 0)
-                    firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, 1)
-                    firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, 0)
-                    firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, 0)
-                end
-            end
-            if i == 8 then
-                if game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
-                    firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, workspace:WaitForChild("BoatStages"):WaitForChild("NormalStages").TheEnd.GoldenChest.Trigger, 1)
-                firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, workspace:WaitForChild("BoatStages"):WaitForChild("NormalStages").TheEnd.GoldenChest.Trigger, 0)
-                firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, workspace:WaitForChild("BoatStages"):WaitForChild("NormalStages").TheEnd.GoldenChest.Trigger, 1)
-            end
-            end
-           gold:FireServer()
-        queue_on_teleport(
-'loadstring(game:HttpGet("https://raw.githubusercontent.com/JustAP1ayer/ignorewtffxd/main/2131.lua", true))()') -- basically the same script repeated
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/JustAP1ayer/ignorewtffxd/main/serverhopxd.lua", true))()
+--[[i hate my life why does it crash ]] repeat wait(0.1) until  game.Loaded and game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart") -- i hate my life why does it crash 
+--[[i hate my life why does it crash ]]                 local gold = workspace:WaitForChild("ClaimRiverResultsGold")  -- i hate my life why does it crash 
+--[[i hate my life why does it crash ]]        game:GetService("RunService").Stepped:connect( -- i hate my life why does it crash 
+--[[i hate my life why does it crash ]]            function() -- i hate my life why does it crash 
+--[[i hate my life why does it crash ]]                    game.Players.LocalPlayer.Character:WaitForChild("Humanoid"):ChangeState(11)
+--[[i hate my life why does it crash ]]            end)
+--[[i hate my life why does it crash ]]
+--[[i hate my life why does it crash ]]                  
+--[[i hate my life why does it crash ]]   gold:FireServer()
+--[[i hate my life why does it crash ]]            local bodyVelocity = Instance.new("BodyVelocity")
+--[[i hate my life why does it crash ]]            bodyVelocity.Velocity = Vector3.new(0, 0, 0)
+--[[i hate my life why does it crash ]]            bodyVelocity.Parent = game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+--[[i hate my life why does it crash ]]             for i = 1, 10 do
+--[[i hate my life why does it crash ]]            game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = workspace:WaitForChild("BoatStages"):WaitForChild("NormalStages")["CaveStage"..i].DarknessPart.CFrame 
+--[[i hate my life why does it crash ]]             wait(2)
+--[[i hate my life why does it crash ]]             end
+--[[i hate my life why does it crash ]]            if i == 5 then
+--[[i hate my life why does it crash ]]                if game:GetService("Players").LocalPlayer.Character  and game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart") and game:GetService("Players").LocalPlayer.Character.Humanoid.Health > 0 then
+--[[i hate my life why does it crash ]]                    firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, 0) -- it works doe!!!
+--[[i hate my life why does it crash ]]                    firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, 0)
+--[[i hate my life why does it crash ]]                    firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, 0)
+--[[i hate my life why does it crash ]]                    firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, 1)
+--[[i hate my life why does it crash ]]                    firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, 0)
+--[[i hate my life why does it crash ]]                    firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, 0)
+--[[i hate my life why does it crash ]]                end
+--[[i hate my life why does it crash ]]            end
+--[[i hate my life why does it crash ]]            if i == 8 then
+--[[i hate my life why does it crash ]]                if game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
+--[[i hate my life why does it crash ]]                    firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, workspace:WaitForChild("BoatStages"):WaitForChild("NormalStages").TheEnd.GoldenChest.Trigger, 1)
+--[[i hate my life why does it crash ]]                firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, workspace:WaitForChild("BoatStages"):WaitForChild("NormalStages").TheEnd.GoldenChest.Trigger, 0)
+--[[i hate my life why does it crash ]]                firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, workspace:WaitForChild("BoatStages"):WaitForChild("NormalStages").TheEnd.GoldenChest.Trigger, 1)
+--[[i hate my life why does it crash ]]            end
+--[[i hate my life why does it crash ]]            end
+--[[i hate my life why does it crash ]]           gold:FireServer()
+--[[i hate my life why does it crash ]]        queue_on_teleport(
+--[[i hate my life why does it crash ]]'loadstring(game:HttpGet("https://raw.githubusercontent.com/JustAP1ayer/ignorewtffxd/main/2131.lua", true))()') -- basically the same script repeated
+--[[i hate my life why does it crash ]]            loadstring(game:HttpGet("https://raw.githubusercontent.com/JustAP1ayer/ignorewtffxd/main/serverhopxd.lua", true))()
