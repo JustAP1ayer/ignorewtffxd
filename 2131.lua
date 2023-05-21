76fafabcaf1347e87794aa438de68cccb6e0edf2
JustAP1ayer <70037178+JustAP1ayer@users.noreply.github.com>
2023-05-21 14:04:06 -0400

    Update 2131.lua

--- 2131.lua
+++ 2131.lua
@@ -1,4 +1,4 @@
--- hop off skid
+
         if not game:IsLoaded() then game.Loaded:Wait() end
 if game.PlaceId == 537413528 and task.wait() then
 repeat wait(0.1) until  game.Loaded and game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart") 
@@ -34,40 +34,41 @@ repeat wait(0.1) until  game.Loaded and game.Players.LocalPlayer.Character and g
     local players = game:GetService("Players")
 local stages = workspace:WaitForChild("BoatStages"):WaitForChild("NormalStages")
 local gold = workspace:WaitForChild("ClaimRiverResultsGold")  
-local LocalPlayer = players.LocalPlayer
-       game:GetService("RunService").Stepped:connect( 
-           function() 
-                   game.Players.LocalPlayer.Character:WaitForChild("Humanoid"):ChangeState(11)
-           end)         
+local LocalPlayer = players.LocalPlayer      
   gold:FireServer()
            local bodyVelocity = Instance.new("BodyVelocity")
-           bodyVelocity.Velocity = Vector3.new(0, 0, 0)
+           bodyVelocity.Velocity = Vector3.new(0, -0.025, 0)
            bodyVelocity.Parent = game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+            bodyVelocity.maxForce = Vector3.new(9e9, 9e9, 9e9)  
+            bodyVelocity.Name = "PlayerHub"
             for i = 1, 10 do
             LocalPlayer.Character.HumanoidRootPart.CFrame = stages["CaveStage" .. i].DarknessPart.CFrame wait((i == 1 and 4 or 2) + 0.1)
-            end
-           if i == 5 then
+            if i == 5 then
                if game:GetService("Players").LocalPlayer.Character  and game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart") and game:GetService("Players").LocalPlayer.Character.Humanoid.Health > 0 then
                    firetouchinterest(workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, 0) -- it works doe!!!
                    firetouchinterest(workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, 0)
+                                       game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame  =  workspace:WaitForChild("BoatStages"):WaitForChild("NormalStages")["TheEnd"].GoldenChest.Trigger.CFrame
                     firetouchinterest(workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, 1)  -- it works doe!!!
                    firetouchinterest(workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, 0) -- it works doe!!!
                    firetouchinterest(workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, 0) -- it works doe!!!
                    firetouchinterest(workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, 1) -- it works doe!!!
+                                       game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame  =  workspace:WaitForChild("BoatStages"):WaitForChild("NormalStages")["TheEnd"].GoldenChest.Trigger.CFrame
                    firetouchinterest(workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, 0) -- it works doe!!!
                end
            end
-           if i == 8 then
+             if i == 8 then
                if game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
                                       firetouchinterest(workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, 0) -- it works doe!!!
                    firetouchinterest(workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, 1) -- it works doe!!!
                    firetouchinterest(workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, 0) -- it works doe!!!
                    firetouchinterest(workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, 1) -- it works doe!!!
+                                       game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame  =  workspace:WaitForChild("BoatStages"):WaitForChild("NormalStages")["TheEnd"].GoldenChest.Trigger.CFrame
                                       firetouchinterest(workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, 0) -- it works doe!!!
                    firetouchinterest(workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, 0) -- it works doe!!!
                    firetouchinterest(workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, 1) -- it works doe!!!
            end
            end
+            end
           gold:FireServer()
        queue_on_teleport(
   'loadstring(game:HttpGet("https://raw.githubusercontent.com/JustAP1ayer/ignorewtffxd/main/2131.lua", true))()') -- basically the same script repeated
