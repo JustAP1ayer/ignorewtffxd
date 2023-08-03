8c528e4e2c520a7981b0981d06294824b8e24e19
JustAP1ayer <70037178+JustAP1ayer@users.noreply.github.com>
2023-08-02 22:19:03 -0400

    Create unsilence.lua

---
+++ unsilence.lua
@@ -0,0 +1,17 @@
+task.wait(2.5)
+ if not game:IsLoaded() then game.Loaded:Wait() end
+  repeat wait(0.1) until  game.Loaded
+  
+  local args = {
+    [1] = "L",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest"):FireServer(unpack(args))
+task.wait(0.5)
+  queue_on_teleport([[
+     if not game:IsLoaded() then game.Loaded:Wait() end
+  repeat wait(0.1) until  game.Loaded and game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart") 
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/JustAP1ayer/ignorewtffxd/main/unsilence.lua"),true))()
+    ]])
+             loadstring(game:HttpGet("https://raw.githubusercontent.com/JustAP1ayer/ignorewtffxd/main/serverhopxd.lua", true))()
