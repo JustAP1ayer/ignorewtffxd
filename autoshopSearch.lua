e0f694a08dc7a271b83ad1d4675d2191091fa056
JustAP1ayer <70037178+JustAP1ayer@users.noreply.github.com>
2023-08-04 14:00:42 -0400

    Update autoshopSearch.lua

--- autoshopSearch.lua
+++ autoshopSearch.lua
@@ -1,19 +1,21 @@
 task.wait(2.5)
  if not game:IsLoaded() then game.Loaded:Wait() end
   repeat wait(0.1) until  game.Loaded
+local remote 
+    for i,v in pairs(game:GetService("ReplicatedStorage"):GetChildren()) do
+   if string.find(v.Name, "out") then 
+remote = v
+ end
+end
+wait(0.5)
 spawn(function()
    repeat wait(0.1) until  game.Loaded and game.Players.LocalPlayer.Character
   game.Players.LocalPlayer.Character:PivotTo(workspace.Board.CFrame - Vector3.new(-5,-7,0))
  end)
   task.spawn(function()
     while task.wait() do
-local args = {
-    [1] = game.Players.LocalPlayer.UserId
-}
-
-game:GetService("ReplicatedStorage"):WaitForChild("\226\160\128\226\160\128\226\160\128\226\160\128\240\159\152\131_outfit_\226\160\129\226\160\128\226" ..
-    "\160\128\226\160\128\226\160\128\226\160\128\226\160\128\226\160\128\226\160\128\226\160\128" ..
-    "\226\160\128\226\160\128"):FireServer(unpack(args))
+    remote:FireServer(game.Players.LocalPlayer.UserId)
+   print(remote)
 end
   end)
 task.wait(30)
