52cba9898ce89255df4693a800e332c07bc40797
JustAP1ayer <70037178+JustAP1ayer@users.noreply.github.com>
2023-08-05 17:46:40 -0400

    Update autopressbuttonserverhop.lua

--- autopressbuttonserverhop.lua
+++ autopressbuttonserverhop.lua
@@ -11,4 +11,5 @@ fireclickdetector(workspace.Button:FindFirstChild("ThePart"):FindFirstChild("Cli
   repeat wait(0.1) until  game.Loaded and workspace.Button and workspace.Button:FindFirstChild("ThePart") and workspace.Button:FindFirstChild("ThePart"):FindFirstChild("ClickDetector")
     loadstring(game:HttpGet(("https://raw.githubusercontent.com/JustAP1ayer/ignorewtffxd/main/autopressbuttonserverhop.lua"),true))()
     ]])
+task.wait(0.1)
              loadstring(game:HttpGet("https://raw.githubusercontent.com/JustAP1ayer/ignorewtffxd/main/serverhopxd.lua", true))()
