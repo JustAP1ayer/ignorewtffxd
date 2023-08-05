0476bc7c0e24e71059edb5c9c96b447966e2c41f
JustAP1ayer <70037178+JustAP1ayer@users.noreply.github.com>
2023-08-05 17:43:11 -0400

    Create autopressbuttonserverhop.lua

---
+++ autopressbuttonserverhop.lua
@@ -0,0 +1,14 @@
+task.wait(1.5)
+ if not game:IsLoaded() then game.Loaded:Wait() end
+  repeat wait(0.1) until  game.Loaded and workspace:FindFirstChild("Button") and workspace.Button:FindFirstChild("ThePart") and workspace.Button:FindFirstChild("ThePart"):FindFirstChild("ClickDetector")
+fireclickdetector(workspace.Button:FindFirstChild("ThePart"):FindFirstChild("ClickDetector"))
+task.wait(0.1)
+fireclickdetector(workspace.Button:FindFirstChild("ThePart"):FindFirstChild("ClickDetector"))
+task.wait(0.5)
+fireclickdetector(workspace.Button:FindFirstChild("ThePart"):FindFirstChild("ClickDetector"))
+  queue_on_teleport([[
+     if not game:IsLoaded() then game.Loaded:Wait() end
+  repeat wait(0.1) until  game.Loaded and workspace.Button and workspace.Button:FindFirstChild("ThePart") and workspace.Button:FindFirstChild("ThePart"):FindFirstChild("ClickDetector")
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/JustAP1ayer/ignorewtffxd/main/autopressbuttonserverhop.lua"),true))()
+    ]])
+             loadstring(game:HttpGet("https://raw.githubusercontent.com/JustAP1ayer/ignorewtffxd/main/serverhopxd.lua", true))()
