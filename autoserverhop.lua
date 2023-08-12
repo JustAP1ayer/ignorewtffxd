68fa89bb95b1de782d6baa769f7d8ff0f45872e8
JustAP1ayer <70037178+JustAP1ayer@users.noreply.github.com>
2023-08-12 12:57:37 -0400

    Create autoserverhop.lua

---
+++ autoserverhop.lua
@@ -0,0 +1,11 @@
+     if not game:IsLoaded() then game.Loaded:Wait() end
+  repeat wait(0.1) until  game.Loaded and game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart") 
+queue_on_teleport([[
+     if not game:IsLoaded() then game.Loaded:Wait() end
+  repeat wait(0.1) until  game.Loaded and game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart") 
+  task.wait(875)
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/JustAP1ayer/ignorewtffxd/main/autoserverhop.lua"),true))()
+    ]])
+  task.wait(875)
+
+             loadstring(game:HttpGet("https://raw.githubusercontent.com/JustAP1ayer/ignorewtffxd/main/serverhopxd.lua", true))()
